56ce336185498e75ef98cbdde0e29b8cebf4b414
YF1999 <1586939317@qq.com>
2021-01-05 20:57:21 +0800

    feat: add innerError getter in InternalError

--- src/InternalError.ts
+++ src/InternalError.ts
@@ -1,7 +1,10 @@
 export class InternalError extends Error {
     protected _name: string;
     protected _message: string;
+
     protected _stack?: string;
+
+    protected _innerError?: Error;
 
     public constructor(message?: string, innerError?: Error) {
         const _err = (super(message) as any) as Error;
@@ -9,6 +12,7 @@ export class InternalError extends Error {
         this._name = this.constructor.name;
         this._message = _err.message;
         this._stack = _err.stack;
+        this._innerError = innerError;
 
         if (innerError && innerError.stack) {
             this._stack = `${this._stack ?? ''}\n--- inner error ---\n${innerError.stack}`;
@@ -25,5 +29,9 @@ export class InternalError extends Error {
 
     public get stack() {
         return this._stack;
+    }
+
+    public get innerError() {
+        return this._innerError;
     }
 }
